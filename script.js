cf5e60b5ff1f408685d1a6f53f9fcfab9fc6f603
Rozaliya Altynbaeva <rozalya03@mail.ru>
2026-03-25 14:53:24 +0300

    typescript class

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 "use strict";
+class ApiAdapted {
+    baseUrl = "https://catfact.ninja";
+    getRandomFact(maxLength) {
+        let url = `${this.baseUrl}/fact?max_length=${maxLength}`;
+        return fetch(url).then(function (resp) {
+            if (!resp.ok) {
+                throw new Error(`Ошибка ${resp.status}`);
+            }
+            return resp.json();
+        });
+    }
+    getListFacts(maxLength, limit) {
+        let url = `${this.baseUrl}/facts?max_length=${maxLength}&limit=${limit}`;
+        return fetch(url).then(function (resp) {
+            return resp.json();
+        });
+    }
+}
+let a = new ApiAdapted();
+a.getRandomFact(50).then(function (res) {
+    console.log(res);
+});
+a.getListFacts(60, 5).then(function (res) {
+    console.log(res.data);
+});
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -3,6 +3,26 @@ class ApiAdapted {
     
     public getRandomFact(maxLength: number) {
         let url = `${this.baseUrl}/fact?max_length=${maxLength}`;
-        return fetch(url)
+        return fetch(url).then(function(resp: Response) {
+            if(!resp.ok) {
+                throw new Error(`Ошибка ${resp.status}`);
+            }
+            return resp.json();
+        });
+    }
+
+    public getListFacts(maxLength: number, limit: number) {
+        let url = `${this.baseUrl}/facts?max_length=${maxLength}&limit=${limit}`;
+        return fetch(url).then(function(resp: Response) {
+            return resp.json();
+        });
     }
 }
+
+let a = new ApiAdapted();
+a.getRandomFact(50).then(function(res) {
+    console.log(res);
+});
+a.getListFacts(60, 5).then(function(res) {
+    console.log(res.data);
+});
