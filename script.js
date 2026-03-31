68994ab072d41957e39ab151abb3ea3c9a9febee
Rozaliya Altynbaeva <rozalya03@mail.ru>
2026-03-31 14:50:44 +0300

    classes done

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
 "use strict";
+class RespClassFact {
+    fact;
+    constructor(obj) {
+        this.fact = obj.fact;
+    }
+}
+class RespClassListFacts {
+    data;
+    constructor(obj) {
+        this.data = obj.data;
+    }
+}
+class Fact {
+    myFact = "";
+}
 class ApiAdapted {
     baseUrl = "https://catfact.ninja";
     getRandomFact() {
         let url = `${this.baseUrl}/fact`;
-        return fetch(url).then(function (resp) {
-            if (!resp.ok) {
-                throw new Error(`Ошибка ${resp.status}`);
+        return fetch(url).then(function (response) {
+            if (!response.ok) {
+                throw new Error(`Ошибка ${response.status}`);
             }
-            return resp.json();
+            return response.json();
+        }).then(function (json) {
+            const resp = new RespClassFact(json);
+            const entityFact = new Fact();
+            entityFact.myFact = resp.fact;
+            return entityFact;
         });
     }
     getListFacts() {
         let url = `${this.baseUrl}/facts`;
-        return fetch(url).then(function (resp) {
-            return resp.json();
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            const resp = new RespClassListFacts(json);
+            const facts = [];
+            for (let i = 0; i < resp.data.length; i++) {
+                const factEntity = new Fact();
+                factEntity.myFact = resp.data[i].fact;
+                facts.push(factEntity);
+            }
+            return facts;
         });
     }
 }
 let a = new ApiAdapted();
-a.getRandomFact().then(function (res) {
-    console.log(res);
+a.getRandomFact().then(function (response) {
+    console.log(response);
 });
-a.getListFacts().then(function (res) {
-    console.log(res.data);
+a.getListFacts().then(function (response) {
+    console.log(response);
 });
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,28 +1,63 @@
+class RespClassFact {
+    public fact: string
+
+    constructor(obj: any) {
+        this.fact = obj.fact;
+    }
+}
+
+class RespClassListFacts {
+    public data: any[]
+
+    constructor(obj: any) {
+        this.data = obj.data
+    }
+}
+
+class Fact {
+    public myFact: string = ""
+}
+ 
 class ApiAdapted {
     private baseUrl = "https://catfact.ninja";
     
-    public getRandomFact() {
+    public getRandomFact(): Promise<Fact> {
         let url = `${this.baseUrl}/fact`;
-        return fetch(url).then(function(resp: Response) {
-            if(!resp.ok) {
-                throw new Error(`Ошибка ${resp.status}`);
+        return fetch(url).then(function(response: Response) {
+            if(!response.ok) {
+                throw new Error(`Ошибка ${response.status}`);
             }
-            return resp.json();
+            return response.json();
+        }).then(function(json: any) {
+            const resp = new RespClassFact(json);
+            const entityFact = new Fact();
+            entityFact.myFact = resp.fact;
+            return entityFact;
         });
     }
 
-    public getListFacts() {
+    public getListFacts(): Promise<Fact[]> {
         let url = `${this.baseUrl}/facts`;
-        return fetch(url).then(function(resp: Response) {
-            return resp.json();
+        return fetch(url).then(function(response: Response) {
+            return response.json();
+        }).then(function(json: any) {
+            const resp = new RespClassListFacts(json);
+            const facts: Fact[] = [];
+            for (let i = 0; i < resp.data.length; i++) {
+                const factEntity = new Fact();
+                factEntity.myFact = resp.data[i].fact;
+                facts.push(factEntity);
+            }
+            return facts;
         });
     }
 }
 
 let a = new ApiAdapted();
-a.getRandomFact().then(function(res) {
-    console.log(res);
+a.getRandomFact().then(function(response) {
+    console.log(response);
 });
-a.getListFacts().then(function(res) {
-    console.log(res.data);
+a.getListFacts().then(function(response) {
+    console.log(response);
 });
+
